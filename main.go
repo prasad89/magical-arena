1bd5016a8cb00efce51ad58c81abaa75fa1f4221
KrishnaAher <kisnaaher68@gmail.com>
2024-07-27 10:27:22 +0530

    Remove debug/print statements

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	// "magical-arena/battle"
 	"magical-arena/player"
 	
@@ -12,9 +10,6 @@ func main() {
     // Create two players
     player1 := player.CreatePlayer(1)
     player2 := player.CreatePlayer(2)
-
-	fmt.Println(player1)
-	fmt.Println(player2)
 
     // Start the magical arena battle
     // battle.MagicalArena(player1, player2)
